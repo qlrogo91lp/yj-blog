bd2ca220d70c3b75e4214388297cafea959e6a51
yjkim91 <qlrogo91lp@gmail.com>
2026-08-06 07:01:04 +0900

    ✨ PostTile3up(3-col 축소 타일) 추가

---
+++ src/components/post/post-tile-3up.tsx
@@ -0,0 +1,12 @@
+import { PostTileVertical } from './post-tile-vertical';
+import type { PostWithCategory, TagSummary } from '@/types';
+
+type Props = {
+  post: PostWithCategory;
+  tags?: TagSummary[];
+  priority?: boolean;
+};
+
+export function PostTile3up(props: Props) {
+  return <PostTileVertical {...props} size="sm" />;
+}
